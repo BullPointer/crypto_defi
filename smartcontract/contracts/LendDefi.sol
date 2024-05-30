1efec0e2335d2619b08a4b1d7ddc0dc30295aac4
White Bull <blueded88@gmail.com>
2024-05-30 01:39:51 -0400

    Updated to standard

--- smartcontract/contracts/LendDefi.sol
+++ smartcontract/contracts/LendDefi.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/erc20/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LendDefi {
     enum DefiStatus {
@@ -10,11 +10,17 @@ contract LendDefi {
         TRANSACTION_COMPLETE
     }
 
+    enum Currency {
+        ETH,
+        LTH,
+        USDC
+    }
+
     struct Lend {
         address borrower;
         uint256 amount;
         uint256 apy;
-        string currency;
+        Currency currency;
         uint256 duration;
         uint256 timestamp;
         DefiStatus status;
@@ -22,9 +28,9 @@ contract LendDefi {
 
     event LendingEvent(address lender, uint256 amount, DefiStatus status);
 
-    mapping(address => Lend[]) lenders;
+    mapping(address => Lend[]) public lenders;
 
-    address owner;
+    address public owner;
 
     uint256 public ethAPY;
     uint256 public lthAPY;
@@ -35,81 +41,48 @@ contract LendDefi {
     }
 
     modifier onlyOwner() {
-        require(owner == msg.sender);
+        require(owner == msg.sender, "Caller is not the owner");
         _;
     }
 
-    function toLowerCase(string memory strings) pure returns (string memory) {
-        bytes memory byteStrings = bytes(strings);
-        bytes memory bytesLowerCase = new bytes(byteStrings.length);
-
-        for (uint256 count = 0; count < byteStrings.length; count++) {
-            if (byteStrings[count] >= 0x41 && byteStrings[count] <= 0x5A) {
-                bytesLowerCase[count] = bytes1(uint8(byteStrings[count]) + 32);
-            } else {
-                bytesLowerCase[count] = byteStrings[count];
-            }
-        }
-
-        return string(bytesLowerCase);
-    }
-
+    // Function for editing all APY at once
     function editAllAPY(
-        string memory _eth,
-        string memory _lth,
-        string memory _usdc,
         uint256 _ethApyValue,
         uint256 _lthApyValue,
         uint256 _usdcApyValue
     ) external onlyOwner {
-        require(
-            toLowerCase(_eth) == "eth" &&
-                toLowerCase(_lth) == "lth" &&
-                toLowerCase(_usdc) == "usdc",
-            "Unidentified error. Make sure currencies are 'eth', 'lth', 'usdc'"
-        );
-
         ethAPY = _ethApyValue;
         lthAPY = _lthApyValue;
         usdcAPY = _usdcApyValue;
     }
 
+    // Function for editing only one APY at a time
     function editSingleAPY(
         string memory _coin,
         uint256 _apyValue
     ) external onlyOwner {
-        require(
-            toLowerCase(_coin) == "eth" ||
-                toLowerCase(_coin) == "lth" ||
-                toLowerCase(_coin) ==
-                "usdc"
-                "Unsupported Currency(make sure to choose any of eth/lth/usdc)"
-        );
-
-        if (toLowerCase(_coin) == "eth") {
+        if (_coin == Currency.ETH) {
             ethAPY = _apyValue;
-        }
-        if (toLowerCase(_coin) == "lth") {
+        } else if (_coin == Currency.LTH) {
             lthAPY = _apyValue;
-        }
-        if (toLowerCase(_coin) == "usdc") {
+        } else if (_coin == Currency.USDC) {
             usdcAPY = _apyValue;
+        } else {
+            revert("Unsupported Currency");
         }
-        return (
-            "Unsupported Currency(make sure to choose any of eth/lth/usdc)"
-        );
     }
 
+    // Functions for lending ETH, LTH, USDC
     function lendETH(uint256 _amount) external payable {
         require(_amount == msg.value, "Amount sent not equal to amount passed");
         require(_amount > 0, "Investment amount should be greater than 0");
 
-        Lend storage lender = Lend(
+        Lend memory lender = Lend(
             msg.sender,
             _amount,
             ethAPY,
-            "ETH",
-            "duration",
+            Currency.ETH,
+            0,
             block.timestamp,
             DefiStatus.TRANSACTION_COMPLETE
         );
@@ -119,20 +92,22 @@ contract LendDefi {
     }
 
     function lendLTH(uint256 _amount, address _lthToken) external {
-        require(_amount == msg.value, "Error in value input(amount)");
-        require(_amount > 0, "amount must not be 0");
-        require(address(_lthToken).balance > _amount, "Insufficient balance");
+        require(_amount > 0, "Amount must not be 0");
         require(
-            _lthToken.transferFrom(msg.sender, address(this), _amount),
+            IERC20(_lthToken).balanceOf(msg.sender) >= _amount,
+            "Insufficient balance"
+        );
+        require(
+            IERC20(_lthToken).transferFrom(msg.sender, address(this), _amount),
             "Error in Transaction"
         );
 
-        Lend storage lender = Lend(
+        Lend memory lender = Lend(
             msg.sender,
             _amount,
             lthAPY,
-            "LTH",
-            "duration",
+            Currency.LTH,
+            0,
             block.timestamp,
             DefiStatus.TRANSACTION_COMPLETE
         );
@@ -143,21 +118,23 @@ contract LendDefi {
     }
 
     function lendUSDC(uint256 _amount, address _usdcToken) external {
-        require(_amount == msg.value, "Error in value input(amount)");
-        require(_amount > 0, "amount must not be 0");
-        require(address(_usdcToken).balance > _amount, "Insufficient balance");
+        require(_amount > 0, "Amount must not be 0");
+        require(
+            IERC20(_usdcToken).balanceOf(msg.sender) >= _amount,
+            "Insufficient balance"
+        );
 
         require(
-            _usdcToken.transferFrom(msg.sender, address(this), _amount),
+            IERC20(_usdcToken).transferFrom(msg.sender, address(this), _amount),
             "Error in Transaction"
         );
 
-        Lend storage lender = Lend(
+        Lend memory lender = Lend(
             msg.sender,
             _amount,
             usdcAPY,
-            "USDC",
-            "duration",
+            Currency.USDC,
+            0,
             block.timestamp,
             DefiStatus.TRANSACTION_COMPLETE
         );
@@ -165,5 +142,18 @@ contract LendDefi {
         lenders[msg.sender].push(lender);
 
         emit LendingEvent(msg.sender, _amount, DefiStatus.TRANSACTION_COMPLETE);
+    }
+
+    // Functions to get the balance of ETH, LTH, and USDC in the contract
+    function getETHBalance() public view returns (uint256) onlyOwner {
+        return address(this).balance;
+    }
+
+    function getLTHBalance(address _lthToken) public view returns (uint256) onlyOwner {
+        return IERC20(_lthToken).balanceOf(address(this));
+    }
+
+    function getUSDCBalance(address _usdcToken) public view returns (uint256) onlyOwner {
+        return IERC20(_usdcToken).balanceOf(address(this));
     }
 }
